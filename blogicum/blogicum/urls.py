468ed53681c686083c0f69bc75fc5ddc9b6f0a83
Olga <olya.chikareva@gmail.com>
2026-08-21 20:05:41 +0300

    sprint 4

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from blog.views import UserRegistrationView
+
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('', include('blog.urls', namespace='blog')),
+                  path('pages/', include('pages.urls', namespace='pages')),
+                  path('auth/', include('django.contrib.auth.urls')),
+                  path('auth/registration/', UserRegistrationView.as_view(), name='registration'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler403 = 'pages.views.csrf_failure'
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
